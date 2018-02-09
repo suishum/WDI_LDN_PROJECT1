5454c9f761efd4df8d100ba4f186e37152437e26
Sui <suii_shum@hotmail.com>
2018-02-09 22:35:07 +0000

    worked out enemy movement logic

--- src/js/app.js
+++ src/js/app.js
@@ -10,24 +10,6 @@
 window.addEventListener('DOMContentLoaded', () => {
   // Level 1
 
-//   function fadeOut() {
-//     const initialOpacity = 1;
-//     const timerId = setInterval(() => {}, );
-//   }
-//   // FUNCTIONS
-//   function fadeOut(element) {
-//     const op = 1;  // initial opacity
-//     const timer = setInterval(function () {
-//         if (op <= 0.1){
-//             clearInterval(timer);
-//             element.style.display = 'none';
-//         }
-//         element.style.opacity = op;
-//         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-//         op -= op * 0.1;
-//     }, 50);
-// }
-
   // GENERATE GRID
   // height & width of grid
   const height = 10;
@@ -48,7 +30,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   // GENERATE WALLS
-  //turn into wallDictionary later
+  // turn into wallDictionary later
   const walls = [2, 12, 15, 16, 17, 22, 47, 48, 49, 51, 52, 53, 77, 83, 87, 93, 97];
   for (let i=0; i<walls.length; i++) {
     cells[walls[i]].classList.add('wall');
@@ -56,17 +38,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // ADD JIMBA TO BOARD
   cells[currentCell].classList.add('jimba');
-  // MAKE JIMBA MOVE & FART -> CAN'T MOVE PAST GRID EDGE & CAN'T MOVE INTO WALLS
+  // MAKE JIMBA MOVE & FART -> CAN'T MOVE PAST GRID EDGE & CAN'T MOVE INTO WALLS, MOVE WITH ARROW KEYS OR WASD. PRESS SPACEBAR TO FART.
   window.addEventListener('keydown', (e) => {
-    if (e.keyCode === 39 ) {
+    if (e.keyCode === 39 || e.keyCode === 68) {
       console.log('right arrow pressed');
       if(currentCell%width !== width-1 && !cells[currentCell+1].classList.contains('wall')){
+        //add trainsition? move right
         cells[currentCell].classList.remove('jimba');
         currentCell += 1;
+        //add transition left to right
         cells[currentCell].classList.add('jimba');
       }
     }
-    if (e.keyCode === 37) {
+    if (e.keyCode === 37 || e.keyCode === 65) {
       console.log('left arrow pressed');
       if(currentCell%width !== 0 && !cells[currentCell-1].classList.contains('wall')){
         cells[currentCell].classList.remove('jimba');
@@ -74,15 +58,15 @@ window.addEventListener('DOMContentLoaded', () => {
         cells[currentCell].classList.add('jimba');
       }
     }
-    if (e.keyCode === 40) {
+    if (e.keyCode === 40 || e.keyCode === 83) {
       console.log('down arrow pressed');
-      if (currentCell < width*height-1 && !cells[currentCell+width].classList.contains('wall')) {
+      if (currentCell < width*height-width && !cells[currentCell+width].classList.contains('wall')) {
         cells[currentCell].classList.remove('jimba');
         currentCell += width;
         cells[currentCell].classList.add('jimba');
       }
     }
-    if (e.keyCode === 38) {
+    if (e.keyCode === 38 || e.keyCode === 87) {
       console.log('up arrow pressed');
       if (currentCell > width-1 && !cells[currentCell-width].classList.contains('wall')) {
         cells[currentCell].classList.remove('jimba');
@@ -103,16 +87,65 @@ window.addEventListener('DOMContentLoaded', () => {
   // ADD ENEMY TO THE BOARD
   cells[enemyCell].classList.add('enemy');
   // ALGORITHM TO MOVE ENEMY
-  window.setInterval(() => {
-    // moving right
-    if(currentCell%width !== width-1 && !cells[currentCell+1].classList.contains('wall')){
-      cells[currentCell].classList.remove('jimba');
-      currentCell += 1;
-      cells[currentCell].classList.add('jimba');
+  const enemyFacing = ['right', 'left', 'up', 'down'];
+  let direction = enemyFacing[0]; //initialise at right
+  const enemyTimer = window.setInterval(() => {
+    // use switch statement to move enemy
+    switch(direction) {
+      case 'right':
+        if(enemyCell%width !== width-1 && !cells[enemyCell+1].classList.contains('wall')) {
+          cells[enemyCell].classList.remove('enemy');
+          enemyCell += 1;
+          cells[enemyCell].classList.add('enemy');
+        } else {
+          console.log(direction);
+          direction = enemyFacing[Math.floor(Math.random() * enemyFacing.length)];
+          console.log(direction);
+        }
+        break;
+      case 'left':
+        if(enemyCell%width !== 0 && !cells[enemyCell-1].classList.contains('wall')) {
+          cells[enemyCell].classList.remove('enemy');
+          enemyCell -= 1;
+          cells[enemyCell].classList.add('enemy');
+        } else {
+          console.log(direction);
+          direction = enemyFacing[Math.floor(Math.random() * enemyFacing.length)];
+          console.log(direction);
+        }
+        break;
+      case 'down':
+        if(enemyCell < width*height-width && !cells[enemyCell+width].classList.contains('wall')) {
+          cells[enemyCell].classList.remove('enemy');
+          enemyCell += width;
+          cells[enemyCell].classList.add('enemy');
+        } else {
+          console.log(direction);
+          direction = enemyFacing[Math.floor(Math.random() * enemyFacing.length)];
+          console.log(direction);
+        }
+        break;
+      case 'up':
+        if(enemyCell > width-1 && !cells[enemyCell-width].classList.contains('wall')) {
+          cells[enemyCell].classList.remove('enemy');
+          enemyCell -= width;
+          cells[enemyCell].classList.add('enemy');
+        } else {
+          console.log(direction);
+          direction = enemyFacing[Math.floor(Math.random() * enemyFacing.length)];
+          console.log(direction);
+        }
+        break;
     }
   }, 1000);
 
-
+  // extra bits
+  // flip sprite when changing direction
+  // add chicken/ fart meter
+  // add theme music
+  // add fart/ bark/ camera sounds
+  // enter konami code to get a full fart boost
+  //
 
 
 });
